62ceb5c23bce07d97480313fcdeecbfb2fe397f7
Oleg Kovalenko <monstrenyatko@gmail.com>
2020-12-22 01:11:27 -0800

    Migrate to common base image

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,27 @@
-FROM alpine:3
+FROM monstrenyatko/alpine
 
-MAINTAINER Oleg Kovalenko <monstrenyatko@gmail.com>
+LABEL maintainer="Oleg Kovalenko <monstrenyatko@gmail.com>"
 
-RUN apk update && apk upgrade && \
-    apk add --no-cache nginx nginx-mod-stream shadow && \
+RUN apk update && \
+    apk add nginx nginx-mod-stream && \
+    # clean-up
     rm -rf /root/.cache && mkdir -p /root/.cache && \
-    rm -rf /tmp/* /var/tmp/* /var/cache/apk/* && \
-    \
+    rm -rf /tmp/* /var/tmp/* /var/cache/apk/* /var/cache/distfiles/* \
     # delete default config
     rm -rf /etc/nginx/nginx.conf && \
-    \
     # forward request and error logs to docker log collector
     ln -sf /dev/stdout /var/log/nginx/access.log && \
     ln -sf /dev/stderr /var/log/nginx/error.log
 
-COPY run.sh /
-RUN chmod +x /run.sh
+ENV APP_NAME="nginx-app" \
+    APP_USERNAME="nginx" \
+    APP_GROUPNAME="nginx"
 
+COPY run.sh /app/
 COPY nginx.conf /etc/nginx/nginx.conf
+RUN chown -R root:root /app
+RUN chmod -R 0744 /app
+RUN chmod 0755 /app/run.sh
 
 VOLUME ["/var/cache/nginx"]
 
@@ -25,5 +29,5 @@ STOPSIGNAL SIGTERM
 
 EXPOSE 80
 
-ENTRYPOINT ["/run.sh"]
+ENTRYPOINT ["/app/run.sh"]
 CMD ["nginx-app", "-g", "daemon off;"]
--- run.sh
+++ run.sh
@@ -1,54 +1,18 @@
-#!/bin/sh
-
-# Debug output
-set -x
+#!/bin/bash
 
 # Exit on error
 set -e
 
-export APP_USERNAME=nginx
-export APP_GROUPNAME=nginx
+# Load functions
+source /scripts/update-app-user-uid-gid.sh
 
-if [ -n "$NGINX_UID" ]; then
-  APP_UID=$NGINX_UID
-fi
+# Debug output
+set -x
 
-if [ -n "$NGINX_GID" ]; then
-  APP_GID=$NGINX_GID
-fi
+update_user_gid $APP_USERNAME $APP_GROUPNAME $APP_GID
+update_user_uid $APP_USERNAME $APP_UID
 
-if [ -n "$APP_GID" ] && [ "$APP_GID" != "$(id $APP_GROUPNAME -g)" ]; then
-  set +e
-  # delete all users using requested GID
-  cut -d: -f1,4 /etc/passwd | grep -w $APP_GID |
-  while read name_gid
-  do
-    name=$(echo $name_gid | cut -d: -f1)
-    deluser $name
-  done
-  # delete group with requested GID
-  delgroup $(getent group $APP_GID | cut -d: -f1)
-  set -e
-  # update GID
-  groupmod --gid $APP_GID $APP_GROUPNAME
-  usermod --gid $APP_GID $APP_USERNAME
-fi
-
-if [ -n "$APP_UID" ] && [ "$APP_UID" !=  "$(id $APP_USERNAME -u)" ]; then
-  set +e
-  # delete all users using requested UID
-  cut -d: -f1,3 /etc/passwd | grep -w $APP_UID |
-  while read name_uid
-  do
-    name=$(echo $name_uid | cut -d: -f1)
-    deluser $name
-  done
-  set -e
-  #
-  usermod --uid $APP_UID $APP_USERNAME
-fi
-
-if [ "$1" = 'nginx-app' ]; then
+if [ "$1" = $APP_NAME ]; then
   shift;
   mkdir -p /var/tmp/nginx /var/cache/nginx /run/nginx
   chown -R nginx /var/tmp/nginx /var/cache/nginx /run/nginx
